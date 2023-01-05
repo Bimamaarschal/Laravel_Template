a19a34b989ee367c1483aa99cb3cc28948688f75
Bimamaarschal <bimamaarschal@gmail.com>
2023-01-05 11:01:24 +0700

    perubahan1

---
+++ resources/views/scr-doc/master-doc.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+  @include('scr-doc/head')
+  <body>
+  @include('scr-doc/nav')
+
+  @yield('konten')
+    
+    <footer class="footer">Scribbler is a free HTML template created exclusively for <a href="https://tympanus.net/codrops/" target="_blank" class="link link--light">Codrops</a>.</footer>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+    <script>hljs.initHighlightingOnLoad();</script>
+    <script src="{{ asset('scr/js/scribbler.js') }}" ></script>
+
+  </body>
+</html>
---
+++ resources/views/scr-landing/nav.blade.php
@@ -0,0 +1,8 @@
+<nav>
+      <div class="logo"></div>
+      <ul class="menu">
+        <div class="menu__item toggle"><span></span></div>
+        <li class="menu__item"><a href="doc.html" class="link link--dark"><i class="fa fa-book"></i> Bima</a></li>
+        <li class="menu__item"><a href="" class="link link--dark"><i class="fa fa-github"></i> Github</a></li>
+      </ul>
+</nav>
